522b04e19a1d1513f2e8d3b8033b8f40e67167b5
DikaN <diogomiguelcandido@gmail.com>
2020-09-24 15:52:13 +0100

    Uploaded Files!

---
+++ disableambientsounds/client.lua
@@ -0,0 +1,4 @@
+Citizen.CreateThread(function()
+	StartAudioScene('CHARACTER_CHANGE_IN_SKY_SCENE')
+	SetAudioFlag("PoliceScannerDisabled", true)
+end)
---
+++ disableambientsounds/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'DikaN1337'
+description 'Disable Ambient Sounds on FiveM'
+version '1.0.0'
+
+client_script 'client.lua'
